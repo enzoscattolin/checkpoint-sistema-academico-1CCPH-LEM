a7aefd23005735ec56e0d51c8492271e60ea80de
labsfiap <aikuku0110@gmail.com>
2026-04-28 11:46:25 -0300

    Adicionado caso 2 - Leticia

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+case 2: // Calcular média
+                if (notasInseridas) {
+                    media = (nota1 + nota2) / 2;
+                    printf("Media calculada: %.2f\n", media);
+                } else {
+                    printf("Insira as notas primeiro!\n");
+                }
+                break;
+                
 case 4: // Exibir resultado
                 printf("\n------ RESULTADO ------\n");
                 printf("Nota 1: %.2f\n", nota1);
