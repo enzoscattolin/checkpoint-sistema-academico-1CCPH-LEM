cef7fa04efafab7b5d86fc9382ea7c616f73d38b
enzoscattolin <enzoscatolini@gmail.com>
2026-04-28 11:39:46 -0300

    add case 4

---
+++ main.c
@@ -0,0 +1,7 @@
+case 4: // Exibir resultado
+                printf("\n------ RESULTADO ------\n");
+                printf("Nota 1: %.2f\n", nota1);
+                printf("Nota 2: %.2f\n", nota2);
+                printf("Media: %.2f\n", media);
+                printf("----------------------\n");
+                break;
